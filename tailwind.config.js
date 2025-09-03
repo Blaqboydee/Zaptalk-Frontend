3f487836a3b3ece148b8d3fe38d9b3758ba1b4b3
Blaqboydee <adeoluwaadegoke005@gmail.com>
2025-09-03 17:59:14 +0100

    added plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}", // include all source files
