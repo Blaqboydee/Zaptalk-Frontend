658f94cc12dbaeb876a16cf05fd421e060aebd91
Blaqboydee <adeoluwaadegoke005@gmail.com>
2025-09-18 04:26:33 +0100

    removed restrictions

--- src/main.jsx
+++ src/main.jsx
@@ -20,9 +20,3 @@ createRoot(document.getElementById("root")).render(
     </AuthProvider>
   </StrictMode>
 );
-
-const loadingScreen = document.getElementById("loading-screen");
-if (loadingScreen) {
-  loadingScreen.classList.add("fade-out");
-  setTimeout(() => loadingScreen.remove(), 300);
-}
