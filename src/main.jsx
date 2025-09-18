2dfd084af204731f7d1149e1a9b556ff99e1af86
Blaqboydee <adeoluwaadegoke005@gmail.com>
2025-09-18 04:18:14 +0100

    working on responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -20,3 +20,9 @@ createRoot(document.getElementById("root")).render(
     </AuthProvider>
   </StrictMode>
 );
+
+const loadingScreen = document.getElementById("loading-screen");
+if (loadingScreen) {
+  loadingScreen.classList.add("fade-out");
+  setTimeout(() => loadingScreen.remove(), 300);
+}
