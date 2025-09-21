f3a9c1f9c875e2fb3f30d646c9bd855e284e1272
Blaqboydee <adeoluwaadegoke005@gmail.com>
2025-09-22 00:56:52 +0100

    fixing chat input

--- src/components/MobileChatModal.jsx
+++ src/components/MobileChatModal.jsx
@@ -20,7 +20,7 @@ const MobileChatModal = ({
   }
 
   return (
-    <div className="fixed inset-0 z-50 h-[60vh]">
+    <div className="fixed inset-0 z-50">
       <div
         className="absolute inset-0 bg-black bg-opacity-50"
         onClick={onClose}
